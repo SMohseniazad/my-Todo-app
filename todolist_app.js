2a838ec676841d02f99457661f9c8276d1414e6b
Saba <Sabamohseni2000@gmail.com>
2022-12-18 15:31:19 +0330

    UpdateStatus

--- todolist_app.js
+++ todolist_app.js
@@ -1,53 +1,53 @@
-// import {v4 as uuidv4} from 'https://cdnjs.cloudflare.com/ajax/libs/uuid/8.3.2/uuid.min.js';
-
-// console.log(uuidv4());
-
-// const { v4: uuidv4 } = require('uuid');
-// let uidCounter = uuidv4();
 const inputBox = document.querySelector(".addnew input"),
 filters = document.querySelectorAll(".filters span"),
 clearAll = document.querySelector(".btn_clear"),
 taskBox = document.querySelector(".task-box"),
 ulBox = document.querySelector(".ultask");
 
-
 let todoArray = JSON.parse(localStorage.getItem('todos')) || [] ;
-
 console.log(todoArray);
 // create li for Tasks
 function showTodos (){
-    const domTodo = todoArray.map((todo)=> {
+    const domTodo = todoArray.map((todo, id)=> { 
+        let isCompleted = todo.status == "completed" ? "checked" : "";
     console.log(todo);
     return (
         `<li class="litask"> 
-            <label>
-                <input class="checked" type="checkbox" oninput="checkBox()">
-                <p class="text"> ${todo.name} </p>
+            <label for="${id}">
+                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${isCompleted}>
+                <p class="${isCompleted}">${todo.name}</p>
                 <button onclick="deleteTask(${todo.id})"><i class="fa fa-close" style="font-size:24px"></i></button>
             </label>
         </li>`)
-})
-ulBox.innerHTML= domTodo.join(" ") ;
+});
+    ulBox.innerHTML= domTodo.join(" ");  
 }
 
 showTodos();
 
-// create function for new task and put ins object then into arrye and save local storage
 
-// let idCounter =1;
-let idCounter = Math.floor(
-    Math.random() * Math.floor(Math.random() * Date.now())
-);
-console.log(idCounter);
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todoArray[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todoArray[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(todoArray))
+
+}
+// create function for new task and put ins object then into arrye and save local storage
 function addNewItems(value){
     
-    // let idCounter = Date.now();
     let idCounter = Math.floor(
         Math.random() * Math.floor(Math.random() * Date.now())
     );
-    console.log(`one ${idCounter}`);
+    // console.log(`one ${idCounter}`);
+
     let userTask = inputBox.value.trim();
-    let taskObject = {id:idCounter , name:userTask };
+    let taskObject = {id:idCounter , name:userTask , status:"pending"};
     if(userTask != ''){
         todoArray.push(taskObject);
         // idCounter = idCounter + 1;
@@ -60,7 +60,6 @@ function addNewItems(value){
         alert("Please Enter a Task");
 
     };
-
     // clear all tasks
 function clearAllTask(){
     let clearTodos = JSON.parse(localStorage.getItem('todos'));
@@ -73,7 +72,6 @@ function clearAllTask(){
     // idCounter = 1;
     // console.log(idCounter);
 }
-
 // delete task
 function deleteTask(todoId) {
     const filterArray = todoArray.filter( (todo) => todo.id != todoId );
@@ -83,9 +81,3 @@ function deleteTask(todoId) {
     showTodos();
 }
 
-// check task
-// function checkBox() {
-//     const checkboxes = document.querySelector('litask');
-//     checkboxes.addEventListener('click',)
-
-// }
